68b07012b696f022bfea50291545cb4f1973aa29
Elias Leers <eliasleers@gmail.com>
2023-12-08 23:23:08 -0800

    Moved Day1 into its own submodule

--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
 module adventofcode
 
 go 1.21
+
+//replace (
+//	example.com/day1 => ./day1
+//)
+//
+//require example.com/day1
--- main.go
+++ main.go
@@ -1,76 +1,11 @@
 package main
 
 import (
-	"bufio"
-	"errors"
+	"adventofcode/challenges/day1"
 	"fmt"
-	"log"
-	"os"
 )
 
 func main() {
-	day1()
-}
-
-func day1() {
-	// open file
-	f, err := os.Open("./ChallengeFiles/1/input.txt")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	defer f.Close()
-
-	// new scanner to read lines individually
-	scanner := bufio.NewScanner(f)
-
-	calibrationSum := 0
-
-	for scanner.Scan() {
-
-		calibrationValue := calcLineCalibrationValue(scanner.Text())
-		calibrationSum += calibrationValue
-
-	}
-	fmt.Println(calibrationSum)
-}
-
-func calcLineCalibrationValue(line string) int {
-	var calibrationDigits []int
-
-	// iterate over each rune, and if it's a number, append it to calibrationDigits
-	for _, char := range line {
-		num, charErr := CharToNum(char)
-		if charErr != nil {
-			// do nothing
-		} else {
-			// append number to calibrationDigits
-			calibrationDigits = append(calibrationDigits, num)
-		}
-	}
-
-	if len(calibrationDigits) > 0 {
-		// first digit is calibrationDigits[0]
-		firstDigit := calibrationDigits[0] * 10
-
-		// second digit is calibrationDigits[-1]
-		lastDigit := calibrationDigits[len(calibrationDigits)-1] // does empty array break here?
-
-		return firstDigit + lastDigit
-	}
-	return 0
-}
-
-// https://codereview.stackexchange.com/a/122931
-// nice solution to determining if a rune is a number, and throwing error if not.
-// thanks peterSO!
-
-var ErrRuneNotInt = errors.New("type: rune was not int")
-
-func CharToNum(r rune) (int, error) {
-	// This is just ascii number comparison, then subtracting 0's ascii value from the resulting int rune's ascii value
-	if '0' <= r && r <= '9' {
-		return int(r) - '0', nil
-	}
-	return 0, ErrRuneNotInt
+	day1Solution := day1.Solve()
+	fmt.Printf("Day 1 Solution:\n%v\n", day1Solution)
 }
